9fc367f867dcde8963bb4010a722ff37839ffade
StackNinja0109 <whitecrow0109@gmail.com>
2025-04-04 14:51:37 +0900

    feat: rakuten api

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,13 @@ API_TOKEN = os.getenv('API_TOKEN', '')
 # YAHOO
 YAHOO_CLIENT_ID = os.getenv('YAHOO_CLIENT_ID', '')
 YAHOO_API_ENDPOINT = "https://shopping.yahooapis.jp/ShoppingWebService/V3"
+
+# RAKUTEN
+RAKUTEN_APP_ID = os.getenv('RAKUTEN_APP_ID', '')
+RAKUTEN_API_ENDPOINT = "https://app.rakuten.co.jp/services/api"
+
+if not YAHOO_CLIENT_ID:
+  raise ValueError("YAHOO_CLIENT_ID environment variables is not set")
+
+if not RAKUTEN_APP_ID:
+  raise ValueError("RAKUTEN_APP_ID environment variables is not set")

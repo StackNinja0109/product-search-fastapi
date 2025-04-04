6cd7e92827b183743beb5fb3b3af965208e985ea
StackNinja0109 <whitecrow0109@gmail.com>
2025-04-04 09:56:04 +0900

    feat: handle search validation

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth import authenticate_token
-from app.models import HelloRequest
-from app.handlers import handle_hello
+from app.models import SearchRequest
+from app.handlers import handle_search
 
 web_app = FastAPI(title="fastapi-skeleton", version="1.0.0", dependencies=[Depends(authenticate_token)])
 
@@ -18,9 +18,9 @@ web_app.add_middleware(
   allow_headers=["*"]
 )
 
-@web_app.post("/v1/hello")
-async def hello(request: HelloRequest):
-  return await handle_hello(request)
+@web_app.post("/v1/search")
+async def search(request: SearchRequest):
+  return await handle_search(request)
 
 if __name__ == "__main__":
   is_reload = os.getenv("APP_ENV", "staging") == "staging"
